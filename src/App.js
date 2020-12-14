381076c884072645a7c11a6b9ae3d9527484ea49
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-14 04:20:19 +0200

    error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-     <h1>To do App</h1>
+     <TodoList />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function TodoForm(props) {
+	const [input, setInput] = useState('');
+
+	const handleChange = e => {
+		setInput(e.target.value);
+	}
+	
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		props.onSubmit({
+			id: Math.floor(Math.random() * 1000),
+			text: input
+		});
+
+		setInput('');
+	};
+
+	return (
+		<form className='todo-form' onSubmit={handleSubmit}>
+			<input className='todo-input'
+				type='text'
+				placeholder='add a todo'
+				value={input}
+				name='text'
+				onChange={handleChange}
+			/>
+			<button className='todo-btn'>Add todo</button>
+		</form>
+	);
+}
+
+export default TodoForm
